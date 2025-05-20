2718b554b2e354fd1d934d99ccc2ccf62eca8c37
chadrakdev <chadrak@outlook.com>
2025-05-20 21:15:45 +0100

    Remove JSON serialisation annotations

--- HoopInsightsAPI/Models/TeamDto.cs
+++ HoopInsightsAPI/Models/TeamDto.cs
@@ -9,10 +9,6 @@ public class TeamDto
     public string Division { get; set; }
     public string City { get; set; }
     public string Name { get; set; }
-    
-    [JsonPropertyName("full_name")]
     public string FullName { get; set; }
-    
-    [JsonPropertyName("abbreviation")]
     public string Abbreviation { get; set; }
 }
