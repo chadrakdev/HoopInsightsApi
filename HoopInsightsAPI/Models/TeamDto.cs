039bcab919cb833b2c591a9d394ee02c7c5fcf07
chadrakdev <chadrak@outlook.com>
2025-05-21 09:50:11 +0100

    Add JsonPropertyName annotation

--- HoopInsightsAPI/Models/TeamDto.cs
+++ HoopInsightsAPI/Models/TeamDto.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace HoopInsightsAPI.Models;
 
 public class TeamDto
@@ -7,6 +9,8 @@ public class TeamDto
     public string Division { get; set; }
     public string City { get; set; }
     public string Name { get; set; }
+    
+    [JsonPropertyName("full_name")]
     public string FullName { get; set; }
     public string Abbreviation { get; set; }
 }
