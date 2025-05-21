9e326aca2d0d8e190a9d8a2dec604a18701cfc92
chadrakdev <chadrak@outlook.com>
2025-05-21 10:04:03 +0100

    Add back jsonOptions to TeamService

--- HoopInsightsAPI/Services/TeamService.cs
+++ HoopInsightsAPI/Services/TeamService.cs
@@ -7,17 +7,22 @@ namespace HoopInsightsAPI.Services;
 public class TeamService : ITeamService
 {
     private readonly IBalldontlieClient _client;
+    private readonly JsonSerializerOptions _jsonOptions;
 
     public TeamService(IBalldontlieClient client)
     {
         _client = client;
+        _jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
     }
     
     public async Task<IEnumerable<TeamDto>> GetTeamsAsync(string? name = null)
     {
         var teamJson = await _client.GetTeamsJsonAsync(name ?? string.Empty);
 
-        var teamResponse = JsonSerializer.Deserialize<TeamsResponseDto>(teamJson)
+        var teamResponse = JsonSerializer.Deserialize<TeamsResponseDto>(teamJson, _jsonOptions)
                            ?? throw new InvalidOperationException("Failed to parse response from teams endpoint.");
 
         return teamResponse.Data;
