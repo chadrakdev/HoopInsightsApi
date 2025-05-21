edaa16f05e6b59f8fa3b8eee40d41cab7ed7850d
chadrakdev <chadrak@outlook.com>
2025-05-21 09:18:59 +0100

    Create TeamServiceTests skeleton

--- HoopInsightsAPI.Tests/Services/TeamServiceTests.cs
+++ HoopInsightsAPI.Tests/Services/TeamServiceTests.cs
@@ -1,6 +1,17 @@
-namespace HoopInsightsAPI.Tests;
+using HoopInsightsAPI.Clients;
+using HoopInsightsAPI.Services;
+using Moq;
+
+namespace HoopInsightsAPI.Tests.Services;
 
 public class TeamServiceTests
 {
-    
+    private readonly Mock<IBalldontlieClient> _clientMock;
+    private readonly TeamService _teamService;
+
+    public TeamServiceTests()
+    {
+        _clientMock = new Mock<IBalldontlieClient>();
+        _teamService = new TeamService(_clientMock.Object);
+    }
 }
