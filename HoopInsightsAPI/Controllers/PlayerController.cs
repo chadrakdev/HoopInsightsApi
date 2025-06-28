1e1eaef80207885815e1f82e43ca2fb9b0dfeda9
chadrakdev <chadrak@outlook.com>
2025-06-28 15:30:13 +0100

    Add PlayerController and GetPlayers call

--- HoopInsightsAPI/Controllers/PlayerController.cs
+++ HoopInsightsAPI/Controllers/PlayerController.cs
@@ -1,6 +1,27 @@
+using HoopInsightsAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace HoopInsightsAPI.Controllers;
 
-public class PlayerController
+[ApiController]
+[Route("api/v1/[controller]")]
+public class PlayerController : ControllerBase
 {
-    
+    private readonly IPlayerService _playerService;
+
+    public PlayerController(IPlayerService playerService)
+    {
+        _playerService = playerService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetPlayers([FromQuery] string? search, [FromQuery] int perPage = 100,
+        [FromQuery] string? cursor = null)
+    {
+        var result = await _playerService.GetPlayersAsync(search, perPage, cursor);
+
+        if (!result.Data.Any()) return NotFound(new { error = "No players found.", status = 404 });
+
+        return Ok(result);
+    }
 }
