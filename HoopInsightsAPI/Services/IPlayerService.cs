75ad24f099da55b6cca24abe76b48ab4ab078a39
chadrakdev <chadrak@outlook.com>
2025-06-28 15:29:33 +0100

    Add Player interface and implementation

--- HoopInsightsAPI/Services/IPlayerService.cs
+++ HoopInsightsAPI/Services/IPlayerService.cs
@@ -1,6 +1,8 @@
+using HoopInsightsAPI.Models;
+
 namespace HoopInsightsAPI.Services;
 
 public interface IPlayerService
 {
-    
+    Task<PlayersResponseDto> GetPlayersAsync(string? search = null, int perPage = 100, string? cursor = null);
 }
